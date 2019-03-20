50df48aa888e233c9cbd5d3a0f5562d66510969e
Colton <gameguy95@users.noreply.github.com>
2019-03-20 09:28:47 -0700

    First attempt at being able to set clock
    Currently doesn't work right. I think I need to move the set code out of the always block.

--- Digital_Clock.srcs/sources_1/new/Digital_Clock.v
+++ Digital_Clock.srcs/sources_1/new/Digital_Clock.v
@@ -21,14 +21,10 @@
 
 module Digital_Clock(
     input clk,
-    input IO_BTN_C,
-    input IO_BTN_U,
-    input IO_BTN_L,
-    input IO_BTN_R,
-    input IO_BTN_D,
-    input [15:0] IO_SWITCH,      // IO Dipswitches; up = 1
+    input IO_BTN_C,IO_BTN_U, IO_BTN_L, IO_BTN_R, IO_BTN_D,
+    //input [15:0] IO_SWITCH,      // IO Dipswitches; up = 1
     output [6:0] IO_SSEG,
-    output IO_SSEG_DP,
+//    output IO_SSEG_DP,
     output [3:0] IO_SSEG_SEL
     );
     
@@ -38,6 +34,7 @@ module Digital_Clock(
     
     /* Data registers */
     reg [3:0] Tens_of_Hours, Hours, Tens_of_Minutes, Minutes, Tens_of_Seconds, Seconds = 0;
+    reg [0:0] current_bit; //currently only minutes and hours
     sevseg display(.clk(clk),
         .binary_input_0(Minutes),
         .binary_input_1(Tens_of_Minutes),
@@ -45,49 +42,91 @@ module Digital_Clock(
         .binary_input_3(Tens_of_Hours),
         .IO_SSEG_SEL(IO_SSEG_SEL),
         .IO_SSEG(IO_SSEG));
-
+    
+    /* Modes */
+    parameter Hours_And_Minutes = 1'b0;
+    parameter Set_Clock = 1'b1;
+    reg [0:0] Current_Mode = Set_Clock;
+    
     always @(posedge clk) begin
-        // count to 60 seconds, increment minutes, then reset seconds to 0
-            // count to 10 Seconds, increment Tens_of_Seconds, then reset Seconds to 0
-            if (counter < max_counter) begin
-                counter <= counter + 1;
-            end else begin
-                counter <= 0;
-                Seconds <= Seconds + 1;
-            end
-            
-            if (Seconds == 10) begin
-                Seconds <= 0;
-                Tens_of_Seconds <= Tens_of_Seconds + 1;
-            end
-            // count to 6 Tens_of_Seconds, increment Minutes, then reset Tens_of_Seconds to 0
-            if (Tens_of_Seconds == 6) begin
-                Tens_of_Seconds <= 0;
-                Minutes <= Minutes + 1;
-            end
-        // count to 60 minutes, increment hours, then reset minutes to 0
-            // count to 10 Minutes, increment Tens_of_Minutes, then reset Minutes to 0
-            if (Minutes == 10) begin
-                Minutes <= 0;
-                Tens_of_Minutes <= Tens_of_Minutes + 1;
-            end
-            // count to 6 Tens_of_Minutes, increment Hours, then reset Tens_of_Minutes to 0
-            if (Tens_of_Minutes == 6) begin
-                Tens_of_Minutes <= 0;
-                Hours <= Hours + 1;
-            end
-        // count to 24 hours, then reset everything to 0
-            // count to 10 Hours, increment Tens_of_Hours, then reset Hours to 0
-            if (Hours == 10) begin
-                Hours <= 0;
-                Tens_of_Hours <= Tens_of_Hours + 1;
-            end
-            // count to 2 Tens_of_Hours. if Tens_of_Hours = 2 and Hours = 4, then reset the time
-            if (Tens_of_Hours == 2) begin
-                if (Hours == 4) begin
-                    Hours <= 0;
-                    Tens_of_Hours <= 0;
+        case(Current_Mode)
+            Hours_And_Minutes: begin
+                if (counter < max_counter) begin
+                    counter <= counter + 1;
+                end else begin
+                    counter <= 0;
+                    Seconds <= Seconds + 1;
                 end
-            end
+                // count to 60 seconds, increment minutes, then reset seconds to 0
+                    // count to 10 Seconds, increment Tens_of_Seconds, then reset Seconds to 0
+                   if (Seconds == 10) begin
+                        Seconds <= 0;
+                        Tens_of_Seconds <= Tens_of_Seconds + 1;
+                    end
+                    // count to 6 Tens_of_Seconds, increment Minutes, then reset Tens_of_Seconds to 0
+                    if (Tens_of_Seconds == 6) begin
+                        Tens_of_Seconds <= 0;
+                        Minutes <= Minutes + 1;
+                    end
+                // count to 60 minutes, increment hours, then reset minutes to 0
+                    // count to 10 Minutes, increment Tens_of_Minutes, then reset Minutes to 0
+                    if (Minutes == 10) begin
+                        Minutes <= 0;
+                        Tens_of_Minutes <= Tens_of_Minutes + 1;
+                    end
+                    // count to 6 Tens_of_Minutes, increment Hours, then reset Tens_of_Minutes to 0
+                    if (Tens_of_Minutes == 6) begin
+                        Tens_of_Minutes <= 0;
+                        Hours <= Hours + 1;
+                    end
+                // count to 24 hours, then reset everything to 0
+                    // count to 10 Hours, increment Tens_of_Hours, then reset Hours to 0
+                    if (Hours == 10) begin
+                        Hours <= 0;
+                        Tens_of_Hours <= Tens_of_Hours + 1;
+                    end
+                    // count to 2 Tens_of_Hours. if Tens_of_Hours = 2 and Hours = 4, then reset the time
+                    if (Tens_of_Hours == 2) begin
+                        if (Hours == 4) begin
+                            Hours <= 0;
+                            Tens_of_Hours <= 0;
+                        end
+                    end
+                end
+            
+            Set_Clock: begin
+                counter <= 0;
+                current_bit <= 0;
+                Seconds <= 0; //currently just sets seconds to 0. I'll work on this after I get setting minutes and hours working.                 
+                if (IO_BTN_C) begin //center button used to commit time set
+                    Current_Mode <= Hours_And_Minutes;
+                end 
+                case (current_bit)
+                    1'b00: begin //minutes
+                        if (IO_BTN_U) begin
+                            Minutes <= Minutes + 1;
+                        end
+                        if (IO_BTN_D) begin
+                            Minutes <= Minutes - 1;
+                        end
+                        if (IO_BTN_L) begin
+                            current_bit <= 1;
+                        end
+                    end
+                    2'b01: begin //hours
+                        if (IO_BTN_U) begin
+                            Hours <= Hours + 1;
+                        end
+                        if (IO_BTN_D) begin
+                            Hours <= Hours - 1;
+                        end
+                        if (IO_BTN_R) begin
+                            current_bit <= 0;
+                        end
+                    end                        
+                endcase
+                end
+        endcase       
+        
     end
 endmodule
